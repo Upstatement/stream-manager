7d18163f20808d8b545d01b6a5fd5cd3e36e6780
= <jared@upstatement.com>
2015-09-01 13:54:55 -0400

    Cleaned-up load process for Timber

--- stream-manager.php
+++ stream-manager.php
@@ -31,15 +31,7 @@ if ( ! defined( 'WPINC' ) ) die;
 //  Dependencies
 //
 ////////////////////////////////////////////
-///
-if (   file_exists( $composer_autoload = __DIR__ . '/vendor/autoload.php' ) /* check in self */
-	|| file_exists( $composer_autoload = WP_CONTENT_DIR.'/vendor/autoload.php') /* check in wp-content */
-	|| file_exists( $composer_autoload = plugin_dir_path( __FILE__ ).'vendor/autoload.php') /* check in plugin directory */
-	|| file_exists( $composer_autoload = get_stylesheet_directory().'/vendor/autoload.php') /* check in child theme */
-	|| file_exists( $composer_autoload = get_template_directory().'/vendor/autoload.php') /* check in parent theme */
-	) {
-	require_once $composer_autoload;
-}
+
 
 // Check if Timber is installed, and include it before any stream manager
 // things are initiated. This is needed for the TimberStream class.
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,6 +6,7 @@ if ( !$_tests_dir ) $_tests_dir = '/tmp/wordpress-tests-lib';
 require_once $_tests_dir . '/includes/functions.php';
 
 function _manually_load_plugin() {
+	require dirname( __FILE__ ) . '/../wp-content/plugins/timber-library/timber.php';
 	require dirname( __FILE__ ) . '/../stream-manager.php';	
 }
 
