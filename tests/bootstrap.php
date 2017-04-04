ef841bdfc19bfd6aae93809276f51196be0d5203
Linda Gorman <lggorman1@gmail.com>
2017-04-04 09:40:31 -0400

    generated test files

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,18 +1,25 @@
 <?php
+/**
+ * PHPUnit bootstrap file
+ *
+ * @package Stream_Manager
+ */
 
-
-$_tests_dir = getenv('WP_TESTS_DIR');
-if ( !$_tests_dir ) $_tests_dir = '/tmp/wordpress-tests-lib';
-
-require_once $_tests_dir . '/includes/functions.php';
-
-function _manually_load_plugin() {
-	require dirname( __FILE__ ) . '/../wp-content/plugins/timber-library/timber.php';
-	require dirname( __FILE__ ) . '/../stream-manager.php';	
-	require dirname( __FILE__ ) . '/../includes/class-stream-manager-admin.php';	
+$_tests_dir = getenv( 'WP_TESTS_DIR' );
+if ( ! $_tests_dir ) {
+	$_tests_dir = '/tmp/wordpress-tests-lib';
 }
 
+// Give access to tests_add_filter() function.
+require_once $_tests_dir . '/includes/functions.php';
+
+/**
+ * Manually load the plugin being tested.
+ */
+function _manually_load_plugin() {
+	require dirname( dirname( __FILE__ ) ) . '/stream-manager.php';
+}
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
+// Start up the WP testing environment.
 require $_tests_dir . '/includes/bootstrap.php';
-require_once('StreamManager_UnitTestCase.php');
